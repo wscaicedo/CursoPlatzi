a9a4e1acbf64c7f73bcd356327f840a70cf8ca51
wscaicedo <williamcaicedo0593@gmail.com>
2022-06-11 15:29:49 -0500

    Mi primer commit de funciones en Javascript

--- Javascript Function.js
+++ Javascript Function.js
@@ -1,12 +1,12 @@
 function seconLangestNumber(numbers) {
-    let first = numbers[0]
+    let primer = numbers[0]
     let second = 0;
-    for (let i=0; i<numbers.length; i++){
-        if (numbers[i] > first) {
-            second = first;
-            first = numbers[i]
+    for (let i=0; i<numbers.length; i++) {
+        if (numbers[i] > primer) {
+            second = primer;
+            primer = numbers[i]
         }
-        if (numbers[i] > second && numbers[i] < first){
+        if (numbers[i] > second && numbers[i] < primer){
             second = numbers[i];
         }
     }
