7f67da8943407879a8b62ebe3d77e3453528e837
wscaicedo <williamcaicedo0593@gmail.com>
2022-06-15 13:45:21 -0500

    Dibujando con canvas

---
+++ canvas/dibujo.js
@@ -0,0 +1,15 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
+dibujarLinea("blue", 200, 300, 25, 98)
+dibujarLinea("green", 56, 78, 250, 200)
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
