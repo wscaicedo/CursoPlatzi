9f5f9e0f17ddd8ba424840d1be9d51f1ca92a3e5
wscaicedo <williamcaicedo0593@gmail.com>
2022-06-15 19:13:08 -0500

    Modificando la identacion

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -6,10 +6,10 @@ dibujarLinea("green", 56, 78, 250, 200)
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.moveTo(xinicial, yinicial);
-  lienzo.lineTo(xfinal, yfinal);
-  lienzo.stroke();
-  lienzo.closePath();
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.moveTo(xinicial, yinicial);
+	lienzo.lineTo(xfinal, yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
 }
